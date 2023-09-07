08c6f43f1dad290c0bd6a131aab056633c59ddc6
Alex H <alex.holder@bigfootds.com>
2023-09-07 19:46:57 +1000

    feat: successful data retrieval via promise

--- promiseRequest.js
+++ promiseRequest.js
@@ -9,14 +9,19 @@ function getRandomPokemon(){
 
 	let ID = 25;
 	let result = fetch(API_BASE_URL + ID).then((response) => {
-		console.log(response);
+		console.log("Response is: " + JSON.stringify(response, null, 4));
 
-		return response;
+		return response.json();
+	})
+	.then(data => {
+		console.log("Data is: " + JSON.stringify(data, null, 4));
+
+		return data;
 	}).catch(error => {
 		console.log(error);
 	});
 
-	console.log("Result of API request is:" + result);
+	// console.log("Result of API request is:" + result);
 
 	// let somePromise = new Promise(resolve, reject => {
 
